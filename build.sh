3d7d4e905efb7f52c3661d7b0a2d2f168194bce6
romw314 <106016361+romw314@users.noreply.github.com>
2024-03-27 18:30:00 +0100

    Better build script

--- build.sh
+++ build.sh
@@ -1,20 +1,41 @@
 #!/bin/bash
 set -e
-rm -rf pkgs dist
+
+detq() {
+	if grep -q "^$1:$name-$tag " ../../pkgs.conf; then
+		q="^$1:$name-$tag "
+	else
+		q="^$1:$name "
+	fi
+}
+
 grep '^repo:' pkgs.conf | while read name repo; do
 	name="${name:5}"
+	echo "=> Building $name"
+	echo "==> Checking cache"
+	if grep -q "^$name$" build_cache.txt; then
+		echo "===> Skipping"
+		continue
+	fi
+	echo "===> Building"
+	rm -rf pkgs/"$name" dist/"$name"-*
 	mkdir -p pkgs/"$name"
 	cd pkgs/"$name"
+	echo "==> Cloning repo"
 	if ! git clone "$repo" . &>../error.log; then
 		cat ../error.log
 		exit 2
 	fi
+	echo "==> Building versions"
 	git tag -l | while read tag; do
-		echo "=> Building $name-$tag"
+		echo "===> Building $name-$tag"
 		if ! (
-			mkdir -vp ../../dist/"$name"-"$tag"
-			git checkout "$tag"
-			if [ -x build.sh ]; then
+			mkdir -p ../../dist/"$name"-"$tag"
+			git checkout -q "$tag"
+			if grep -q -E "^make:$name(-$tag| )" ../../pkgs.conf; then
+				detq make
+				grep "$q" ../../pkgs.conf | tail -n1 | sed -E 's/^make:[A-Za-z0-9_-]+ //' | sh
+			elif [ -x build.sh ]; then
 				./build.sh
 			elif [ -f build.sh ]; then
 				sh build.sh
@@ -41,8 +62,11 @@ grep '^repo:' pkgs.conf | while read name repo; do
 				make
 			elif [ -f Makefile -o -f makefile -o -f GNUmakefile ]; then
 				make
+			else
+				echo >&2 "ERR: Cannot determine build system for $name-$tag"
 			fi
-			cat ../../pkgs.conf | grep "^dist:$name" | cut -f2,3 -d' ' | while read src out; do
+			detq dist
+			grep "$q" ../../pkgs.conf | cut -f2,3 -d' ' | while read src out; do
 				cp -r "$src" ../../dist/"$name"-"$tag"/"$out"
 			done
 		) &>../error.log; then
@@ -50,5 +74,7 @@ grep '^repo:' pkgs.conf | while read name repo; do
 			exit 1
 		fi
 	done
-	cd ..
+	cd ../..
+	echo "==> Updating build cache"
+	echo "$name" >> build_cache.txt
 done
